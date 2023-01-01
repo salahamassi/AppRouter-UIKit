f727feebe5414143e2d3783df6ac8f202b4f3322
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-01-01 14:06:51 +0200

    just UIViewController can be routable

--- Sources/AppRouter/Routable.swift
+++ Sources/AppRouter/Routable.swift
@@ -5,9 +5,16 @@
 //  Created by Salah Amassi on 21/12/2020.
 //
 
-import Foundation
+import UIKit
 
-public protocol Routable {
+public protocol Routable where Self: UIViewController {
     
     var router: AppRouter? { get }
+}
+
+public extension Routable {
+    
+    var innerRouter: AppRouter? {
+        router?.nestedRouters[self]
+    }
 }
